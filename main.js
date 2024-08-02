bc092449d5f70a999808182d3543b4ca661f6c80
haniaa83 <160784404+haniaa83@users.noreply.github.com>
2024-08-02 18:17:40 +0330

    add file

---
+++ main.js
@@ -0,0 +1,57 @@
+//console.log("hiiii")
+
+//let l = [1,2,"hiii"]
+//for(let i of l)
+//console.log(i)
+
+
+/*let l = [1,2,"hiii"]
+for(let i of l)
+{
+    if(typeof (i) == "string")
+    (
+console.log(i)
+)
+}    برای اینکه جنسش استرینگ بشع*/
+
+
+
+
+/*let obj = {
+    "ali":18,
+    "hossein":20,
+    "hasaan":17,
+
+}*/
+
+
+
+/*for(let[k, v]  of Object.entries(obj))
+console.log(v)*/
+
+
+
+/*let s = "hello" + " " + "alii";
+اسم ها رو میچسبونه*/
+
+
+
+/*let n = "alii"
+let s = "hii to you ${n}"
+console.log(s)*/
+
+
+
+/*let path1 = "Disk 1:/users/hania/Desktop/data.txt";
+fs.renamesync(path1) */
+
+
+/*let obj = {
+    ali:18,
+    hossein:20,
+     hasaan:17,
+
+}
+let s = JSON.stringify(obj, null, 2)*/ 
+//  برای تبدیل فایل به جیسون به صورت مرتب 
+
---
+++ test.js
@@ -0,0 +1,27 @@
+//console.log("hiiii")
+
+//let x = true
+//console.log(x)
+
+//if(2 > 1)
+//{
+    //console.log("hiiii")
+//}
+
+//let x = 20
+//if(x > 10)
+//{
+    //console.log("B10")
+//}
+//else if(x > 8)
+//{
+    //console.log("B8")
+//}
+//else if(x > 4)
+//{
+    //console.log("B4")
+//}
+//else
+//{
+    //console.log("small")
+//}
